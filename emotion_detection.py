ddb6fb273d37a8a61c80ff46e5031280475b147b
ditbenick <nickgeertsema@gmail.com>
2025-08-12 04:19:11 -0400

    update

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-/')
 def emotion_detector(text_to_analyze):
     URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
